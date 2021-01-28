42df3801058e06f80553fbd388cb60fd9e81c222
Haky86 <hakamelassouad@gmail.com>
2021-01-28 22:40:12 +0100

    hi3660-common: Drop HotwordEnrollment
    Signed-off-by: Haky86 <hakamelassouad@gmail.com>

--- hi3660.mk
+++ hi3660.mk
@@ -76,7 +76,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions_hi3660.xml:system/etc/permissions/privapp-permissions_hi3660.xml
 
 # Radio
 PRODUCT_PACKAGES += \
